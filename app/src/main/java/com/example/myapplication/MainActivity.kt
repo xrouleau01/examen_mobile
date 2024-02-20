7caee03dfc84d79b7ddd2a4e6fa217e2240f949e
info1 <6195429@cegepoutaouais.qc.ca>
2024-02-20 12:01:58 -0500

    Finale

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+
+@SuppressLint("StaticFieldLeak")
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val intent = Intent(this, confirmation::class.java)
+
+        val termes = View.OnClickListener {
+            binding.bReserver.isEnabled = binding.switch1.isChecked
+        }
+
+        val reserver = View.OnClickListener {
+            var pass = true
+            try {
+                val nbJours = binding.eTNbJours.text.toString().toInt()
+                intent.putExtra("nbJours", nbJours.toString())
+            }
+            catch (_: NumberFormatException) {
+                Toast.makeText(this, "Veuillez entrer un nombre de jours valide", Toast.LENGTH_LONG).show()
+                pass = false
+            }
+            finally {
+                val tag: String = "Main Activity"
+                Log.i(tag, binding.spinModele.selectedItem.toString())
+                Log.i(tag, binding.spinCouleur.selectedItem.toString())
+                Log.i(tag, binding.eTNbJours.text.toString())
+                binding.eTNbJours.text.clear()
+                binding.switch1.isChecked = false
+                binding.bReserver.isEnabled = false
+                if (pass) {
+                    intent.putExtra("modele", binding.spinModele.selectedItem.toString())
+                    intent.putExtra("couleur", binding.spinCouleur.selectedItem.toString())
+                    startActivity(intent)
+                }
+            }
+        }
+        binding.switch1.setOnClickListener(termes)
+        binding.bReserver.setOnClickListener(reserver)
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/confirmation.kt
+++ app/src/main/java/com/example/myapplication/confirmation.kt
@@ -1,11 +1,49 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.example.myapplication.databinding.ActivityConfirmationBinding
+import com.example.myapplication.databinding.ActivityMainBinding
 
+@SuppressLint("StaticFieldLeak")
+lateinit var binding2: ActivityConfirmationBinding
 class confirmation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_confirmation)
+        binding2 = ActivityConfirmationBinding.inflate(layoutInflater)
+        setContentView(binding2.root)
+
+        val tag2: String = "Confirmation Activity"
+        Log.i(tag2, intent.getStringExtra("modele").toString())
+        Log.i(tag2, intent.getStringExtra("couleur").toString())
+        Log.i(tag2, intent.getStringExtra("nbJours").toString())
+
+        val quitter = View.OnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Confirmer")
+            builder.setMessage("Êtes-vous sûr de vouloir quitter?")
+            builder.setPositiveButton("Oui") { _, _ ->
+                finish()
+            }
+            builder.setNegativeButton("Non") { dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.setCancelable(true)
+
+            val dialog = builder.create()
+            dialog.show()
+        }
+
+        binding2.bQuitter.setOnClickListener(quitter)
+
+        val nbJours = intent.getStringExtra("nbJours")?.toInt()
+        var total = "$ "
+        total += ((nbJours?.times(72)).toString())
+        binding2.tVCout.text = total
     }
+
 }
